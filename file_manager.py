8f381a0270fd3bfc4736cb2e1f6dd25af34f2b3d
Rediet Ayele <reayele@gmail.com>
2026-07-15 01:29:43 -0400

    Added the load saved contacts feature

--- file_manager.py
+++ file_manager.py
@@ -16,3 +16,28 @@ def save_contacts(contacts):
     file = open("contacts.json", "w")
     json.dump(contact_list, file)
     file.close()
+
+from contact import Contact
+
+def load_contacts():
+    try:
+        file = open("contacts.json", "r")
+        contact_list = json.load(file)
+        file.close()
+
+        contacts = []
+
+        for contact_data in contact_list:
+            contact = Contact(
+                contact_data["name"],
+                contact_data["phone"],
+                contact_data["email"]
+            )
+
+            contacts.append(contact)
+        return contacts
+    
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
